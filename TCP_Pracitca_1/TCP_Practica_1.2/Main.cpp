3bdbfb32acead1dfb2912dd2b05b367eebd28980
StormValen <valentin.g.l@gmail.com>
2018-02-16 01:09:16 +0100

    Items 1 -> 3 done

--- TCP_Pracitca_1/TCP_Practica_1.2/Main.cpp
+++ TCP_Pracitca_1/TCP_Practica_1.2/Main.cpp
@@ -9,22 +9,30 @@
 #define MAX_MENSAJES 30
 sf::TcpSocket socket;
 sf::Socket::Status status;
+bool done;
+
+// ----- BLOCKING THREAD ----- //
 void thread_recived(std::vector<std::string> *aMensajes) {
-	while (true) {
+	bool tBucle = true;
+	while (tBucle) {
 		std::size_t received;
 		char buffer_Thread[2000];
 		status = socket.receive(buffer_Thread, sizeof(buffer_Thread), received);
-		//std::cout << buffer_Thread << std::endl;
-		aMensajes->push_back(buffer_Thread);
+		if (buffer_Thread == ">exit") {
+			aMensajes->push_back("Connexion Finished");
+			tBucle = false;
+		}
+		else {
+			aMensajes->push_back(buffer_Thread);
+		}
 	}
 }
 
 int main()
 {
+	// ----- WINDOW UI ----- //
 	std::vector<std::string> aMensajes;
-
 	sf::Vector2i screenDimensions(800, 600);
-
 	sf::RenderWindow window;
 	window.create(sf::VideoMode(screenDimensions.x, screenDimensions.y), "Chat");
 
@@ -35,7 +43,6 @@ int main()
 	}
 
 	sf::String mensaje = " >";
-
 	sf::Text chattingText(mensaje, font, 14);
 	chattingText.setFillColor(sf::Color(0, 160, 0));
 	chattingText.setStyle(sf::Text::Bold);
@@ -50,90 +57,112 @@ int main()
 	separator.setFillColor(sf::Color(200, 200, 200, 255));
 	separator.setPosition(0, 550);
 
-
-
 	sf::IpAddress ip = sf::IpAddress::getLocalAddress();
 
+	//  ----- SELECTION MODE ----- //
 	char connectionType, mode;
+	std::string executingMode;
 	char buffer[2000];
 	std::size_t received;
 	std::string Stext = "Connected to: ";
-	char _mode;
-	//std::cout << "1 --> Blocking + Threading \n 2--> NonBlocking \n 3-->Blocking + SocketSelector" << std::endl;
-	//std::cin >> _mode;
 
 	std::cout << "Enter (s) for Server, Enter (c) for Client: ";
 	std::cin >> connectionType;
-	/*if (connectionType == 'c') {
-		std::cout << "Enter the server's ip: ";
-		std::cin >> ip; //192.168.122.143
-	}*/
 
 	if (connectionType == 's')
 	{
+		std::cout << "Enter (b) for Blocking + Threading, Enter (n) for NonBlocking, Enter (s) for Blocking + SocketSelector: " << std::endl;
+		std::cin >> executingMode;
 
 		sf::TcpListener listener;
 		status = listener.listen(50000);
-		status = listener.accept(socket);
+		status = listener.accept(socket); //Blocking
 		Stext += "Server";
 		mode = 's';	
 		listener.close();
+		
+		//Se envia el modo de ejecucion al cliente.
+		socket.send(executingMode.c_str(), executingMode.length() + 1);
 	}
 	else if (connectionType == 'c')
 	{
-		status = socket.connect("192.168.122.2", 50000);
+		status = socket.connect("192.168.1.40", 50000);
 		Stext += "Client";
 		mode = 'r';
+
+		//Se recive el modo de ejecucion desde el server.
+		socket.receive(buffer, sizeof(buffer), received);
+		executingMode = buffer;
+
+		if(executingMode == "b"){
+			std::cout << "INFO: (b) Blocking + Threading" << std::endl;
+		}
+		else if (executingMode == "n") {
+			std::cout << "INFO: (n) NonBlonking" << std::endl;
+		}
+		else if (executingMode == "s") {
+			std::cout << "INFO: (s) Bloking + Socket Selector" << std::endl;
+		}
 		
 	}
-
+	 // ----- CONFIRM CONNECTION ----- //
 	socket.send(Stext.c_str(), Stext.length() + 1);
 	socket.receive(buffer, sizeof(buffer), received);
-	
 	std::cout << buffer << std::endl;
 
-	std::thread tr(&thread_recived, &aMensajes);
-
-	bool done = false;
-	while (!done)
-	{
-		while (window.isOpen())
+	// ----- MODOS DE EJECUCION ----- // 
+	if (executingMode == "b") { //Blocking +  Thread
+		std::thread tr(&thread_recived, &aMensajes);
+		done = false;
+		while (!done)
 		{
-			sf::Event evento;
-			while (window.pollEvent(evento))
+			while (window.isOpen())
 			{
-				switch (evento.type)
+				sf::Event evento;
+				while (window.pollEvent(evento))
 				{
-				case sf::Event::Closed:
-					window.close();
-					break;
-				case sf::Event::KeyPressed:
-					if (evento.key.code == sf::Keyboard::Escape)
-						window.close();
-					else if (evento.key.code == sf::Keyboard::Return)
+					switch (evento.type)
 					{
-						//SEND			
+					case sf::Event::Closed:
+						window.close();
+						break;
+					case sf::Event::KeyPressed:
+						if (evento.key.code == sf::Keyboard::Escape)
+							window.close();
+						else if (evento.key.code == sf::Keyboard::Return)
+						{	
 							Stext = mensaje;
-							status = socket.send(Stext.c_str(), Stext.length() + 1);
-							aMensajes.push_back(mensaje);
+							std::cout << Stext << std::endl;
 
-						if (aMensajes.size() > 25)
-						{
-							aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+
+							if (Stext != ">exit") {
+								
+								status = socket.send(Stext.c_str(), Stext.length() + 1);
+								aMensajes.push_back(mensaje);
+								if (aMensajes.size() > 25)
+								{
+									aMensajes.erase(aMensajes.begin(), aMensajes.begin() + 1);
+								}
+								mensaje = ">";
+							}
+							else {
+								done = true;
+								socket.disconnect();
+								return 0;
+							}
+							
 						}
-						mensaje = ">";
+						break;
+					case sf::Event::TextEntered:
+						if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
+							mensaje += (char)evento.text.unicode;
+						else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
+							mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
+						break;
 					}
-					break;
-				case sf::Event::TextEntered:
-					if (evento.text.unicode >= 32 && evento.text.unicode <= 126)
-						mensaje += (char)evento.text.unicode;
-					else if (evento.text.unicode == 8 && mensaje.getSize() > 0)
-						mensaje.erase(mensaje.getSize() - 1, mensaje.getSize());
-					break;
 				}
-			}
-			window.draw(separator);
-			//RECIVE
+
+				window.draw(separator);
 				for (size_t i = 0; i < aMensajes.size(); i++)
 				{
 					std::string chatting = aMensajes[i];
@@ -141,17 +170,24 @@ int main()
 					chattingText.setString(chatting);
 					window.draw(chattingText);
 				}
-			//mensaje = "";
-			std::string mensaje_ = mensaje + "_";
-			text.setString(mensaje_);
-			window.draw(text);
+				std::string mensaje_ = mensaje + "_";
+				text.setString(mensaje_);
+				window.draw(text);
 
 
-			window.display();
-			window.clear();
+				window.display();
+				window.clear();
+			}
 		}
+		
 	}
-
+	else if (executingMode == "n") { //NonBloking
+		
+	}
+	else if (executingMode == "s") { //Socket Selector
+		
+	}
+	
 	socket.disconnect();
 	return 0;
 }
