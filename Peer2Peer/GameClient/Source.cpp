a57b75eb5e32fe5773aa5ddc4b57d2a7218fc05c
joanlojo <joanlojo.7@gmail.com>
2018-03-09 20:14:13 +0100

    proyexto peer

---
+++ Peer2Peer/GameClient/Source.cpp
@@ -0,0 +1,16 @@
+#include <SFML\Network.hpp>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <thread>
+#include <mutex>
+#include <cstring>
+#include <SFML\Graphics.hpp>
+
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ Peer2Peer/GameServer/Source.cpp
@@ -0,0 +1,14 @@
+#include <SFML\Network.hpp>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <thread>
+#include <mutex>
+#include <cstring>
+#include <SFML\Graphics.hpp>
+
+int main()
+{
+
+	return 0;
+}
