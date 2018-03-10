77d1a4c9e4a044a756dc10c0c9e19ba89c779661
joanlojo <joanlojo.7@gmail.com>
2018-03-10 01:00:08 +0100

    peer2peer error

--- Peer2Peer/GameClient/Source.cpp
+++ Peer2Peer/GameClient/Source.cpp
@@ -6,11 +6,56 @@
 #include <mutex>
 #include <cstring>
 #include <SFML\Graphics.hpp>
+#define MAX_PLAYERS 4
+std::vector<sf::TcpSocket*> aPeers;
 
-
+struct Direction //almazenar la direccion de cada peer
+{
+	std::string IP;
+	unsigned short port;
+};
+std::vector<Direction> aStr;
 int main()
 {
-
-
+	sf::TcpSocket sock;
+	sf::Socket::Status status;
+	status = sock.connect("192.168.1.34", 50000); //te conectas con el bootstrap
+	if (status != sf::Socket::Done) {
+		std::cout << "Error" << std::endl;
+	}
+	sf::Packet packet;
+	Direction direction;
+	status = sock.receive(packet); //recives las ip y puertos
+	sock.disconnect();
+	if (status != sf::Socket::Done) {
+		std::cout << "Error" << std::endl;
+	}
+	int prob = 0;
+	packet >> prob;
+	for (int i = 0; i <prob ; i++) {
+		packet >> direction.IP >> direction.port;
+		aStr.push_back(direction); //añades las ip y puertos al vector
+	}
+	for (int i = 0; i < aStr.size(); i++) {
+		sf::TcpSocket *sock = new sf::TcpSocket; // creas un nuevo socket para conectarte con cada peer
+		status = sock->connect(aStr[i].IP, aStr[i].port);
+		if (status != sf::Socket::Done) {
+			std::cout << "Error" << std::endl;
+		}
+		aPeers.push_back(sock);
+	}
+	if (aPeers.size() < MAX_PLAYERS) {
+		sf::TcpListener listener;
+		status = listener.listen(sock.getLocalPort()); //si aun no hay 4 jugadores escuchas por tu puerto para tener mas conexiones
+		if (status != sf::Socket::Done) {
+			std::cout << "Error" << std::endl;
+		}
+		for (int i = aPeers.size(); i > MAX_PLAYERS; i--) {
+			sf::TcpSocket *sockNew = new sf::TcpSocket;
+			listener.accept(*sockNew); //????????????
+			aPeers.push_back(sockNew);
+		}
+		listener.close();
+	}
 	return 0;
 }
--- Peer2Peer/GameServer/Source.cpp
+++ Peer2Peer/GameServer/Source.cpp
@@ -7,8 +7,51 @@
 #include <cstring>
 #include <SFML\Graphics.hpp>
 
+#define MAX_PLAYERS 4
+
+struct Direction //almazenar la direccion de cada peer
+{
+	sf::String IP;
+	unsigned short port;
+};
+std::vector<Direction> aPeers; //almazenar los peers
+
 int main()
 {
-
+	sf::TcpListener listener;
+	sf::Socket::Status status =  listener.listen(50000);
+	if (status != sf::Socket::Done) {
+		std::cout << "Conection error" << std::endl;
+	}
+	for (int i = 0; i < MAX_PLAYERS; i++) {
+		sf::TcpSocket sock;
+		status = listener.accept(sock);
+		if (status != sf::Socket::Done) {//creas la conexion con quien quiera conectarse, los peers
+			std::cout << "Error" << std::endl;
+		}
+		else {
+			Direction direction;
+			if (aPeers.empty()) {
+				//No hay nadie conectado, tienes que añadir la ip y puerto del primer peer pero no le pasas nada
+				direction.IP = sock.getRemoteAddress().toString();
+				direction.port = sock.getRemotePort();
+				aPeers.push_back(direction);
+			}
+			else {
+				sf::Packet packet;
+				//tienes que enviar al peer las ip y puertos de los otros peers ya conectados
+				packet << aPeers.size();
+				for (int i = 0; i < aPeers.size(); i++) {
+					packet << aPeers[i].IP << aPeers[i].port;
+				}
+				sock.send(packet);// envias el paquete con la ip y el puerto
+				direction.IP = sock.getRemoteAddress().toString();
+				direction.port = sock.getRemotePort();
+				aPeers.push_back(direction); //añader el nuevo peer que te ha pedido conexion
+			}
+		}
+		sock.disconnect();
+	}
+	listener.close();
 	return 0;
 }
