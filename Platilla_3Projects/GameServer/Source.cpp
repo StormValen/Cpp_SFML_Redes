ad828dbb38da16869fb087a8de0b0c132da6cabb
StormValen <valentin.g.l@gmail.com>
2018-03-14 00:55:24 +0100

    server logic , not finished yet

--- Platilla_3Projects/GameServer/Source.cpp
+++ Platilla_3Projects/GameServer/Source.cpp
@@ -9,12 +9,15 @@
 #define MAX_PLAYERS 4
 
 std::list<sf::TcpSocket*> myClients; //Lista con todos los clientes conectados.
+std::string currentState = "chat";
+int counterForChat = 0;
 
 class Player {
 public:
 	sf::TcpSocket* sock;
 	std::string nickname;
 	int money, bet, betMoney;
+	bool isReady;
 
 	Player() {
 
@@ -25,15 +28,40 @@ public:
 		nickname = "";
 		money = 100;
 		bet = betMoney = 0;
+		isReady = false;
 	}
 };
 
-std::vector<Player> aPlayers; // Contenedor de jugadores
+std::list<Player*> aPlayers; // Contenedor de jugadores
 int clientsConnectedCounter = 0;
+bool gameIsReady = false;
+
+void thread_game() {
+	bool tBucle = true;
+	while (tBucle) {
+		counterForChat++;
+		if (counterForChat == 5000) {
+			tBucle = false;
+			currentState = "notChat";
+			std::cout << "Chat finished" << std::endl;
+		}
+	}
+}
+
+bool ArePlayersReady() {
+	for (std::list<Player*>::iterator it = aPlayers.begin(); it != aPlayers.end(); it++) {
+		Player& player = **it;
+		std::cout << player.nickname << player.isReady << std::endl;
+		if (player.isReady == false) {
+			return false;
+		}
+	}
+	return true;
+}
 
 void SocketSelector() {
 	bool end = false;
-	bool gameIsReady = false;
+	
 	char name[200];
 
 	// -- Abrir listener
@@ -55,41 +83,45 @@ void SocketSelector() {
 		if (mySocketSelector.wait()) {
 			if (mySocketSelector.isReady(listener) && !gameIsReady) {
 				sf::TcpSocket* newClient = new sf::TcpSocket;
+				Player* newPlayer = new Player(newClient,"");
 
 				std::size_t received;
-				if (listener.accept(*newClient) == sf::Socket::Done) {
+				if (listener.accept(*newPlayer->sock) == sf::Socket::Done) {
 					//Bucle para todos los clientes -> Nuevo cliente conectado.
 					//Antes de añadir el nuevo cliente para no tener que comparalos.
 
-					/*for (std::list<sf::TcpSocket*>::iterator it = myClients.begin(); it != myClients.end(); it++) {
-						std::string connectedMesage = "--- A new client has been connected ---";
-						sf::TcpSocket& clientSocket = **it;
-						clientSocket.send(connectedMesage.c_str(), connectedMesage.length() + 1);
-					}*/
+					newPlayer->sock->receive(name, sizeof(name), received);
 
-					myClients.push_back(newClient);
-					newClient->receive(name, sizeof(name), received);
+					std::cout << "Client with port: [" << newPlayer->sock->getRemotePort() << "] and name [" << name << "] CONNECTED" << std::endl;
+					mySocketSelector.add(*newPlayer->sock);
+					newPlayer->nickname = name;
 
-					std::cout << "Client with port: [" << newClient->getRemotePort() << "] and name [" << name << "] CONNECTED" << std::endl;
-					mySocketSelector.add(*newClient);
-
-					Player newPlayer(newClient,name);
 					aPlayers.push_back(newPlayer);
 
 					std::string confirmText = "INFO: You have entered the game, please wait until all players are connected ...";
 					size_t bytesSent;
-					newClient->send(confirmText.c_str(), confirmText.length());
+					newPlayer->sock->send(confirmText.c_str(), confirmText.length());
+
+					for (std::list<Player*>::iterator it = aPlayers.begin(); it != aPlayers.end(); it++) {
+						Player& iPlayer = **it;
+						std::string connectedMesage = "GAME INFO: " + newPlayer->nickname + " has been connected ---";
+						iPlayer.sock->send(connectedMesage.c_str(), connectedMesage.length() + 1);
+					}
 
 					clientsConnectedCounter++;
 
 					if (clientsConnectedCounter == 4) {
 						gameIsReady = true;
 						std::cout << "All 4 player are connected ... Starting Game ..." << std::endl;
-						for (std::list<sf::TcpSocket*>::iterator it = myClients.begin(); it != myClients.end(); it++) {
-							std::string connectedMesage = "All 4 player are connected ... Starting Game ...";
-							sf::TcpSocket& clientSocket = **it;
-							clientSocket.send(connectedMesage.c_str(), connectedMesage.length() + 1);
+
+						for (std::list<Player*>::iterator it = aPlayers.begin(); it != aPlayers.end(); it++) {
+							Player& iPlayer = **it;
+							std::string connectedMesage = "GAME INFO: All 4 player are connected ... Starting Game ...";
+							iPlayer.sock->send(connectedMesage.c_str(), connectedMesage.length() + 1);
 						}
+
+						//std::thread tr(&thread_game);
+						//tr.join();
 					}
 				}
 				else {
@@ -98,52 +130,65 @@ void SocketSelector() {
 				}
 			}
 			else if (gameIsReady) {
-				for (std::list<sf::TcpSocket*>::iterator it = myClients.begin(); it != myClients.end(); it++) {
-					sf::TcpSocket& clientSocketReceive = **it;
-					if (mySocketSelector.isReady(clientSocketReceive)) {
-						
+				if (currentState == "chat") {
+					for (std::list<Player*>::iterator it = aPlayers.begin(); it != aPlayers.end(); it++) {
+						Player& iPlayer = **it;
+						if (mySocketSelector.isReady(*iPlayer.sock)) {
+
 
 							//Server recibe mensajes
 							std::string mesage;
 							char buffer[2000];
 							std::size_t received;
-							status = clientSocketReceive.receive(buffer, sizeof(buffer), received);
+							status = iPlayer.sock->receive(buffer, sizeof(buffer), received);
 							if (status == sf::Socket::Done) {
 								mesage = buffer;
-								std::cout << "Client with port: [" << clientSocketReceive.getRemotePort() << "] SEND: " << mesage << std::endl;
+								std::cout << "Client with port: [" << iPlayer.sock->getRemotePort() << "] SEND: " << mesage << std::endl;
+								if (mesage == ">ready") {
+									iPlayer.isReady = true;
+								}
 							}
 
 							//Cliente desconectado
 							else if (status == sf::Socket::Disconnected) {
-								mySocketSelector.remove(clientSocketReceive);
+								mySocketSelector.remove(*iPlayer.sock);
 								//eliminar el socket de la lista
-								std::cout << "Client with port: [" << clientSocketReceive.getRemotePort() << "] DISCONECTED " << std::endl;
+								std::cout << "Client with port: [" << iPlayer.sock->getRemotePort() << "] DISCONECTED " << std::endl;
 
-								for (std::list<sf::TcpSocket*>::iterator it = myClients.begin(); it != myClients.end(); it++) {
-									std::string connectedMesage = "--- A client has been disconected ---";
-									sf::TcpSocket& clientSocket = **it;
-									clientSocket.send(connectedMesage.c_str(), connectedMesage.length() + 1);
+								for (std::list<Player*>::iterator it = aPlayers.begin(); it != aPlayers.end(); it++) {
+									Player& bPlayer = **it;
+									std::string connectedMesage = "GAME INFO: " + iPlayer.nickname + " left the game ---";
+									bPlayer.sock->send(connectedMesage.c_str(), connectedMesage.length() + 1);
+								}
+
+							}
+
+							//Server reenvia mensajes
+							for (std::list<Player*>::iterator it = aPlayers.begin(); it != aPlayers.end(); it++) {
+								Player& player = **it;
+								if (player.sock->getRemotePort() != iPlayer.sock->getRemotePort()) {
+									player.sock->send(mesage.c_str(), mesage.length() + 1);
 								}
 							}
 
-							for (std::list<sf::TcpSocket*>::iterator it = myClients.begin(); it != myClients.end(); it++) {
-								sf::TcpSocket& clientSocket = **it;
-								if (clientSocket.getRemotePort() != clientSocketReceive.getRemotePort()) {
-									clientSocket.send(mesage.c_str(), mesage.length() + 1);
-								}
+							if (ArePlayersReady()) {
+								currentState = "notChat";
 							}
+							
 						}
-						else {
-							for (std::list<sf::TcpSocket*>::iterator it = myClients.begin(); it != myClients.end(); it++) {
-								std::string connectedMesage = "Wait until a new player connects";
-								sf::TcpSocket& clientSocket = **it;
-								clientSocket.send(connectedMesage.c_str(), connectedMesage.length() + 1);
-							}
-						}
+					}
+				}
+				else if (currentState == "notChat") {
+					for (std::list<Player*>::iterator it = aPlayers.begin(); it != aPlayers.end(); it++) {
+						Player& player = **it;
+						std::string mesage = "Chatting time has finished";
+						player.sock->send(mesage.c_str(), mesage.length() + 1);
+						
 					}
 				}
 			}
 		}
+	}
 	listener.close();
 	mySocketSelector.clear();
 	}
@@ -151,6 +196,14 @@ void SocketSelector() {
 int main()
 {
 	SocketSelector();
+	
+	std::thread counter(&thread_game);
+	counter.join();
+	
+
+	
+
+
 	system("pause");
 	return 0;
 }
